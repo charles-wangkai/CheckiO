9fd2689d5a4fc3cc8ea502e069c9caa766861afe
Kai Wang <charles.wangkai@gmail.com>
2015-07-19 00:32:46 +0800

    Moore Neighbourhood

---
+++ count-neighbours.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def count_neighbours(grid, row, col):
+    return len(list(filter(lambda neighbour: 0 <= neighbour[0] < len(grid) and 0 <= neighbour[1] < len(grid[0]) and grid[neighbour[0]][neighbour[1]] == 1, [(row + dr, col + dc) for dc in range(-1, 2) for dr in range(-1, 2) if dr != 0 or dc != 0])))
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
