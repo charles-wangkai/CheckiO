6162856cd7973c2f5c5288f5a2a36e7a6be546a4
Kai Wang <charles.wangkai@gmail.com>
2015-07-21 16:06:22 +0800

    The best number ever

---
+++ the-best-number-ever.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def checkio():
+    'And the best number ever is... 0, because it is the only number which is palindrome written in any base, even in the sense of either upside down, rotate, or mirror in the case of LED seven-segment display :-p'
+    return 0
