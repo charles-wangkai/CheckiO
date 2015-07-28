961909f33874fe6b45061bf62f65cfecff1d6980
Kai Wang <charles.wangkai@gmail.com>
2015-07-28 19:13:37 +0800

    Binary count

---
+++ binary-count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def checkio(number):
+    return len(list(filter(lambda digit: digit == '1', bin(number)[2:])))
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
