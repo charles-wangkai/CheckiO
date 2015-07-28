ead2eed4dbbe464f3fb6f053855ca34a3a946bdc
Kai Wang <charles.wangkai@gmail.com>
2015-07-28 19:07:08 +0800

    Pangram

---
+++ pangram.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import string
+
+def check_pangram(text):
+    return all(map(lambda letter: letter in text.lower(), string.ascii_lowercase))
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
