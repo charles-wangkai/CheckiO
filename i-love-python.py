c3c96a97cd9106fc456f9973efd06381f1a06e41
Kai Wang <charles.wangkai@gmail.com>
2015-07-30 17:54:18 +0800

    I love Python!

---
+++ i-love-python.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def i_love_python():
+    """
+        Let's explain why do we love Python.
+    """
+    'I love Python because of its cleanness, elegance, and strong power!'
+    return "I love Python!"
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert i_love_python() == "I love Python!"
